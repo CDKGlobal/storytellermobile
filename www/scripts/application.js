16fd68832ae5ee106fcb5c0894ef8b08303f0d87
Lum, Kimberly <Kimberly.Lum@cdk.com>
2015-07-09 16:06:17 -0700

    refreshes ever 5 seconds to receive new messages

--- www/scripts/application.js
+++ www/scripts/application.js
@@ -2,16 +2,22 @@ angular
 .module('SteroidsApplication', ['supersonic'])
 
 .controller('MessageController', function($scope, supersonic, $http) {
-// 		// //['supersonic'] is a dependency of SteroidsApplication
-	$http.jsonp("http://fleet.ord.cdk.com/storytellerconsumer/messages?callback=JSON_CALLBACK")
-	.success(function(data, status, headers, config, scope) {
-		supersonic.logger.log("Success! " + status);
-		$scope.messages = data;
-	})
-	.error(function(data, status, headers, config) {
-		supersonic.logger.log("Error: " + status);
-		$scope.wLat = "Error: no connection";
-	});
+	// ['supersonic'] is a dependency of SteroidsApplication
+	$scope.getMsgs = function() {
+
+		$http.jsonp("http://fleet.ord.cdk.com/storytellerconsumer/messages?callback=JSON_CALLBACK")
+		.success(function(data, status, headers, config, scope) {
+			supersonic.logger.log("Success! " + status);
+			$scope.messages = data;
+			$scope.apply;
+		})
+		.error(function(data, status, headers, config) {
+			supersonic.logger.log("Error: " + status);
+			$scope.wLat = "Error: no connection";
+		});
+	}
+
+	setInterval($scope.getMsgs, 5000);
 });
 
 // .factory('IndexFactory', function IndexFactory ($http) {
