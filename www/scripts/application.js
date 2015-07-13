fabbd784d648b279390b92daa33e2a1b4b5d35e6
Lum, Kimberly <Kimberly.Lum@cdk.com>
2015-07-13 08:46:25 -0700

    change variable names, inserted loading message

--- www/scripts/application.js
+++ www/scripts/application.js
@@ -3,12 +3,12 @@ angular
 
 .controller('MessageController', function($scope, supersonic, $http) {
 	// ['supersonic'] is a dependency of SteroidsApplication
+	$scope.allMsg = {"messages":[{"userId":"Loading...","message":"Please wait"}]};
 	$scope.getMsgs = function() {
 
 		$http.jsonp("http://fleet.ord.cdk.com/storytellerconsumer/messages?callback=JSON_CALLBACK")
 		.success(function(data, status, headers, config, scope) {
-			supersonic.logger.log("Success! " + status);
-			$scope.messages = data;
+			$scope.allMsg = data;
 			$scope.apply;
 		})
 		.error(function(data, status, headers, config) {
