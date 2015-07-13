eaeab3a1435fb9b88cddebefc1ab45d2b12db6ce
Lum, Kimberly <Kimberly.Lum@cdk.com>
2015-07-13 16:52:12 -0700

    moved js from index.html to external file

--- dist/app/consumer.js
+++ dist/app/consumer.js
@@ -44,3 +44,27 @@ angular
 		$scope.apply;
 	}
 });
+window.onload = function() {
+	var topButton = document.getElementById("topNavButton");
+	window.addEventListener("scroll", function (oEvent) {
+		// var mydivpos = document.getElementById("allMessages").offsetTop;
+		var height = window.innerHeight;
+		if(window.pageYOffset > height) {
+			topButton.style.display = "block";
+		} else {
+			topButton.style.display = "none";
+		}
+	});
+}
+var timeOut;
+function scrollToTop() {
+	if (document.body.scrollTop!=0 || document.documentElement.scrollTop!=0) {
+		window.scrollBy(0,-50);
+		timeOut=setTimeout('scrollToTop()',10);
+	} else {
+		clearTimeout(timeOut);
+	}
+               // show rotating thinger at the top
+               // reload page
+                // hide the thinger
+}
