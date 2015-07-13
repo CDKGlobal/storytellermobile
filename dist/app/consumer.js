b3feb9442834bae4abdff5094955ee8d67e3b49b
Lum, Kimberly <Kimberly.Lum@cdk.com>
2015-07-13 16:14:17 -0700

    applied controller to all of index.html to solve scope problems

--- dist/app/consumer.js
+++ dist/app/consumer.js
@@ -17,19 +17,29 @@ angular
 .controller('MessageController', function($scope, supersonic, $http) {
 	// ['supersonic'] is a dependency of SteroidsApplication
 	$scope.allMsg = {"messages":[{"userId":"Loading...","message":"Please wait"}]};
-	
-	$scope.getMsgs = function() {
 
-		$http.jsonp("http://fleet.ord.cdk.com/storytellerconsumer/messages?callback=JSON_CALLBACK")
+
+	$http.jsonp("http://fleet.ord.cdk.com/storytellerconsumer/messages?callback=JSON_CALLBACK")
+	.success(function(data, status, headers, config, scope) {
+		$scope.allMsg = data;
+		$scope.apply;
+	})
+	.error(function(data, status, headers, config) {
+		supersonic.logger.log("Error: " + status);
+		$scope.wLat = "Error: no connection";
+	});
+
+	$scope.update = function () {
+		supersonic.logger.log("updating...");
+		$http.jsonp("https://api.forecast.io/forecast/f9bc1181900dcc4b7555f6e48e4998a7/37.8267,-122.423?callback=JSON_CALLBACK")
 		.success(function(data, status, headers, config, scope) {
+			supersonic.logger.log("Success! " + status);
 			$scope.allMsg = data;
 			$scope.apply;
 		})
 		.error(function(data, status, headers, config) {
 			supersonic.logger.log("Error: " + status);
-			$scope.wLat = "Error: no connection";
+			$scope.allMsg = {"messages":[{"userId":"Error!","message":"Please restart the app."}]};
 		});
 	}
-
-	setInterval($scope.getMsgs, 5000);
 });
