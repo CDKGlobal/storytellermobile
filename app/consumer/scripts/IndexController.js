c3245515ba6ca3f11af44688e68d76ca9b7d734e
Lum, Kimberly <Kimberly.Lum@cdk.com>
2015-07-24 11:00:40 -0700

    updated search fields, changed API query url

--- app/consumer/scripts/IndexController.js
+++ app/consumer/scripts/IndexController.js
@@ -3,7 +3,7 @@ angular
 	// Declare any module-specific AngularJS dependencies here
 	'common'
 ])
-.controller('MessageController', function($scope, supersonic, $http, $sce) {
+.controller('MessageController', function($scope, supersonic, $http) {
 	$scope.index = { spinner: false };
 
 	$scope.update = function () {
@@ -19,45 +19,23 @@ angular
 			supersonic.logger.log("Error: " + status);
 		});
 	}
-
 	$scope.update();
-
-	$scope.modLink = function(message) {
-		// John Gruber's regex, modified for JS
-		var regex = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/i;
-		var modified = message.replace(regex, "<a onclick=\"supersonic.app.openURL('$1')\" href=\"\">$1</a>");
-		console.log(modified);
-		return $sce.trustAsHtml(modified);
-	}
 })
 .controller('SearchController', function($scope, supersonic, $http) {
 	// $scope.results = { hide: true };
 	$scope.found = { none: true };
 	$scope.search = function() {
 		document.activeElement.blur();
-		var user = $scope.search.userId;
-		var mcontent = $scope.search.msgcontent;
-		var filters = $scope.search.filters;
+		var keywords = $scope.search.keywords;
 		var start = $scope.search.startdate;
 		var end = $scope.search.enddate;
 		var contentQuery = "";
-		var filterQuery = "";
 
-		if(!angular.isDefined(user)) {
-			user = "";
-		}
-		if(angular.isDefined(mcontent)) {
-			var contentParams = mcontent.match(/\w+|"(?:\\"|[^"])+"/g);
+		if(angular.isDefined(keywords)) {
+			var contentParams = keywords.match(/\w+|"(?:\\"|[^"])+"/g);
 			contentQuery = contentParams[0];
 			for(var i = 1; i < contentParams.length; i++) {
 				contentQuery += "," + contentParams[i];
-			}
-		}
-		if(angular.isDefined(filters)) {
-			var filterParams = filters.split(" ");
-			filterQuery = filterParams[0];
-			for(var i = 1; i < filterParams.length; i++) {
-				filterQuery += "," + filterParams[i];
 			}
 		}
 		if(!angular.isDefined(start)) {
@@ -67,8 +45,7 @@ angular
 			end = "";
 		}
 
-		var url = "http://fleet.ord.cdk.com/storytellerconsumer/messages?query=message=" + encodeURIComponent(contentQuery)
-			 + "%26tags=" + encodeURIComponent(filterQuery) + "&callback=JSON_CALLBACK";
+		var url = "http://fleet.ord.cdk.com/storytellerconsumer/messages?query=" + encodeURIComponent(contentQuery) + "&callback=JSON_CALLBACK";
 		console.log(url);
 
 		$http.jsonp(url)
@@ -84,5 +61,14 @@ angular
 			.error(function(data, status, headers, config) {
 				supersonic.logger.log("Search error: " + status);
 			});
+	}
+})
+.controller('LinkController', function($scope, supersonic, $sce) {
+	$scope.modLink = function(message) {
+		// John Gruber's regex, modified for JS
+		var regex = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/i;
+		var modified = message.replace(regex, "<a onclick=\"supersonic.app.openURL('$1')\" href=\"\">$1</a>");
+		console.log(modified);
+		return $sce.trustAsHtml(modified);
 	}
 });
