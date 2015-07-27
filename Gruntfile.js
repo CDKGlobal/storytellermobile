e3fc85ea1c87e68aea55ffc80e660fa708b17169
Carter, Katherine <Katherine.Carter@cdk.com>
2015-07-27 13:45:31 -0700

    tested controllers for app, added Karma, Jasmine, and Grunt dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,19 @@ Licensed under the MIT license.
 
 module.exports = function(grunt) {
   grunt.loadNpmTasks("grunt-steroids");
+  grunt.loadNpmTasks('grunt-karma');
+
+  grunt.initConfig({
+  	karma: {
+  		unit: {
+  			configFile: 'test/karma.conf.js'
+  		}
+  	}
+  })
+
   grunt.registerTask("default", [
     "steroids-make-fresh"
   ]);
+
+  grunt.registerTask("unittest", ["karma"]);
 }
