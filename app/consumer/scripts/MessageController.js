4d3a2d790424185e14e90915b0e925bbfa99fd10
Lum, Kimberly <Kimberly.Lum@cdk.com>
2015-08-19 16:23:22 -0700

    added onclick property to links right when data is received

--- app/consumer/scripts/MessageController.js
+++ app/consumer/scripts/MessageController.js
@@ -1,6 +1,12 @@
 angular.module('consumer')
 .controller('MessageController', function($scope, supersonic, $http, urlPrefix, modTimestamp, $timeout, allStoriesService, validateService, increaseAmount, dateService) {
 	var count;
+	// John Gruber's regex, modified for JS
+	var linkRegex = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/i;
+	var linkExp = new RegExp(linkRegex);
+	var anchorRegex = /<a[^>]*>([^<]+)<\/a>/gi;
+	var anchorExp = new RegExp(anchorRegex);
+	var txtRegex = />.*</;
 
 	$scope.index = {spinner: false};
 	$scope.stories = {hide: true};
@@ -72,7 +78,7 @@ angular.module('consumer')
 		var promise = $http.jsonp(baseUrl)
 		.success(function(data, status, headers, config, scope) {
 			supersonic.logger.log("Success! updating " + status);
-			$scope.allMsg = data;
+			$scope.allMsg = $scope.processMessages(data);
 			if(data == null || data.messages.length === 0) {
 				$scope.hideMoreButton = true;
 				$scope.noMore = true;
@@ -98,6 +104,28 @@ angular.module('consumer')
 
 	$scope.modTime = function(oldStamp) {
 		return modTimestamp.modTime(oldStamp);
+	}
+
+	// add onclick to all links in messages
+	$scope.processMessages = function(allData) {
+		var modifiedData = [];
+		var tempData = allData.messages;
+		for(var i = 0; i < tempData.length; i++) {
+			var tempStory = tempData[i];
+			var message = tempStory.message;
+			if(anchorExp.test(message)) {
+				while((result = anchorExp.exec(message)) !== null && result[0].indexOf("<a onclick") < 0) {
+					var text = result[0].match(txtRegex);
+					var modLink = "<a onclick=\"supersonic.app.openURL(\'" + linkExp.exec(result[0])[0] +"\')\" href=\"\"" + text + "/a>";
+					supersonic.logger.log("message.indexOf(result[0]): " + message.indexOf(result[0]))
+					message = message.replace(result[0], modLink);
+				}
+			}
+			tempStory.message = message;
+			modifiedData.push(tempStory);
+		}
+		var wrapArr = {messages: modifiedData};
+		return wrapArr;
 	}
 
 });
